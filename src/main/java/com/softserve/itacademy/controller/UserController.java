463363b332c4e170ab9acdda59bd5580d788f6ae
ZmeyOdessit <rrozumovich@gmail.com>
2020-10-31 16:52:04 +0200

    Created create-user.html and controller.

--- src/main/java/com/softserve/itacademy/controller/UserController.java
+++ src/main/java/com/softserve/itacademy/controller/UserController.java
@@ -2,13 +2,13 @@ package com.softserve.itacademy.controller;
 
 import com.softserve.itacademy.dto.UserDto;
 import com.softserve.itacademy.dto.UserTransformer;
+import com.softserve.itacademy.model.Role;
 import com.softserve.itacademy.model.User;
 import com.softserve.itacademy.service.RoleService;
 import com.softserve.itacademy.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,26 +18,27 @@ public class UserController {
     private final UserService userService;
     private final RoleService roleService;
 
-
     public UserController(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
     }
 
-    //add needed fields
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("user", new UserDto());
+        return "create-user";
+    }
 
-    //    @GetMapping("/create")
-//    public String create(//add needed parameters) {
-//        //ToDo
-//        return " ";
-//    }
-//
-//    @PostMapping("/create")
-//    public String create(//add needed parameters) {
-//        //ToDo
-//        return " ";
-//    }
-//
+    @PostMapping("/create")
+    public String create(@Validated @ModelAttribute("user") UserDto userDto, BindingResult result) {
+        userDto.setRole(roleService.readById(2));
+        if (result.hasErrors()) {
+            return "create-user";
+        }
+        User newUser = UserTransformer.convertToUser(userDto);
+        userService.create(newUser);
+        return "redirect:/users/all";
+    }
 
     @GetMapping("/{id}/read")
     public String read(@PathVariable long id, Model model) {
@@ -50,6 +51,7 @@ public class UserController {
     public String update(@PathVariable long id, @RequestParam("oldPassword") String oldPassword,
                          @RequestParam("roleId") long roleId, Model model,
                          @Validated @ModelAttribute("user") UserDto userDto, BindingResult result) {
+        System.out.println("ID==" + id);
         User user = UserTransformer.convertToUser(userDto);
         System.out.println("USER ==" + user.toString());
         User oldUser = userService.readById(id);
@@ -60,6 +62,7 @@ public class UserController {
             return "update-user";
         }
         System.out.println(user.toString());
+        user.setRole(roleService.readById(roleId));
         userService.update(user);
         //System.out.println("USVER2 ==" + userService.readById(id).getRole().toString());
         return "redirect:/users/" + id + "/read";
@@ -74,17 +77,15 @@ public class UserController {
         return "update-user";
     }
 
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable("id") long id) {
+        userService.delete(id);
+        return "redirect:/users/all";
+    }
 
-
-//    @GetMapping("/{id}/delete")
-//    public String delete(//add needed parameters) {
-//        //ToDo
-//        return " ";
-//    }
-//
-//    @GetMapping("/all")
-//    public String getAll(//add needed parameters) {
-//        //ToDo
-//        return " ";
-//    }
+    @GetMapping("/all")
+    public String getAll(Model model) {
+        model.addAttribute("users", userService.getAll());
+        return "users-list";
+    }
 }
